a5fb99cb61b9b9e995c34a489cb6d0ddc7704c1d
Victor Achocalla <vachocalla@gmail.com>
2026-04-06 03:50:10 -0400

    Cambio mensaje

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 async def read_root():
-    return {"message": "Hola Mundo desde K8s y ArgoCD!"}
+    return {"message": "Hola Mundo desde K8s y ArgoCD! Wii"}
 
 @app.get("/health")
 async def health_check():
